8d55f8721b44d4ccc1267e9f1461a6df08e91966
Steve Downey <sdowney2@bloomberg.net>
2026-05-25 12:05:33 -0400

    Step 26: negative compile tests

---
+++ src/smd/schemepoc/test_neg_static_vector.cpp
@@ -0,0 +1,13 @@
+#include <smd/schemepoc/static_vector.hpp>
+
+int main() {
+    using namespace smd::schemepoc;
+    constexpr auto overflow = [] {
+        static_vector<int, 2> vec{};
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3); // Overflow
+        return vec.size();
+    }();
+    return overflow;
+}
